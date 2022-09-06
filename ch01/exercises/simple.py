aea1d29c8b390d4419abed8afbf07e293233a4de
Joe <jmichau1@binghamton.edu>
2022-09-06 17:44:36 -0400

    I added code to simple.py

--- ch01/exercises/simple.py
+++ ch01/exercises/simple.py
@@ -1,1 +1,7 @@
-print()
+print(10 * 5, 10 ** 2, 15 / 10, 15 // 10, -15 // 10, 15 % 10, 10 % 15, 10 % 10, 0 % 10, 10 / 15)
+# 10/15 goes on forever the computer stops the mathimatical equation
+rate = int(input("What is the current exchange rate for the Euro to Dollar?: "))
+amount = int(input("How much money would you like to exchage today?: "))
+total = rate * amount
+result =  (rate - 3)
+print(result)
