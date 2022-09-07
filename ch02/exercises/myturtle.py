212bbad3ee868682fa83b22b0931f110b87b308a
Joe <jmichau1@binghamton.edu>
2022-09-07 17:44:07 -0400

    I added code to myturtle.py

--- ch02/exercises/myturtle.py
+++ ch02/exercises/myturtle.py
@@ -5,5 +5,14 @@ Window = turtle.Screen()
 MyTurtle.shape("turtle")
 MyTurtle.color("purple")
 Length = 50
-
+for i in range (4):
+    MyTurtle.forward(50)
+    MyTurtle.left(90)
+MyTurtle.up()
+MyTurtle.backward(100)
+MyTurtle.color("red")
+MyTurtle.down()
+for i in range (4):
+    MyTurtle.forward(50)
+    MyTurtle.left(90)
 Window.exitonclick()
