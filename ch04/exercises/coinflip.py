94c3b1e708637e17c6c5f9b5ba09836347529eef
Joseph Michaud <112440110+JosephMichaud1@users.noreply.github.com>
2022-10-04 00:49:25 +0000

    Added code to coinflip.py

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -1,5 +1,22 @@
 import random
 import turtle
-coinFlip = random.randint(0,1)
+coinFlip = random.randint(1,3)
 randomTurtle = turtle.Turtle()
+window = turtle.Screen()
+forward = 0
+backward = 0
+while forward or backward != 200:
+  if coinFlip == 1:
+    for i in range(4):
+      randomTurtle.forward(50)
+      forward += 50
+      print("Tails")
+    break
+  else:
+    randomTurtle.backward(50)
+    backward += 50
+    print("Heads")
 
+
+
+window.exitonclick()
