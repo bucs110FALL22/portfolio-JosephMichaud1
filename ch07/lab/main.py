15a0dadde1426ae99336192d1e502279aad104c0
Joe <jmichau1@binghamton.edu>
2022-11-04 20:13:13 -0400

    Finshed LAB 7

--- ch07/lab/main.py
+++ ch07/lab/main.py
@@ -1,6 +1,6 @@
 from Rectangle import Rectangle
+from Rectangle import Rectangle
 from Surface import Surface
-
 def main():
     print("###################  Test #1: Testing Rectangle Class ####################")
     r = Rectangle(10, 10, 10, 10)
@@ -15,7 +15,7 @@ def main():
     assert((r.x, r.y, r.height, r.width) == (1,1,1,0))
     print(r)
     print("Test Complete!")
-
+ 
     print("###################  Test #2: Test Surface Class ####################")
     s = Surface("myimage.png", 10, 10, 10, 10)
     assert((s.rect.x, s.rect.y, s.rect.height, s.rect.width) == (10,10,10,10))
