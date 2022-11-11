93906432c5ed6c166bc0ced34dd17ef6028a96a5
Joe <jmichau1@gmail.com>
2022-11-11 16:55:48 -0500

    asdasdfasdfasdf

--- ch08/lab/StringUtility.py
+++ ch08/lab/StringUtility.py
@@ -4,7 +4,12 @@ class StringUtility:
     def __str__(self):
         return self.string
     def vowels(self):
-        
+        vowles = "a" or "e" or "i" or "o" or "u"
+        vowleCount = 0
+        if vowles in self.string:
+            vowleCount += 1
+            if vowleCount > 3:
+                    return f"many"
     def bothEnds(self):
 
     def fixStart(self):
