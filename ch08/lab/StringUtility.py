ecef7875ee70e8d1439b01bed2810d4c8cfff594
Joe <jmichau1@binghamton.edu>
2022-11-11 16:05:36 -0500

    sdfgsdfgsdfg

---
+++ ch08/lab/StringUtility.py
@@ -0,0 +1,14 @@
+class StringUtility:
+    def __init__(self,string):
+        self.string = string
+    def __str__(self):
+        return self.string
+    def vowels(self):
+        
+    def bothEnds(self):
+
+    def fixStart(self):
+
+    def asciiSum(self):
+
+    def cipher(self):
